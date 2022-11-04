acd006ec8fd621787f9801b8c17993dc8c9fc62d
VascoDaKrava <kravchuk.vasilii@yandex.ru>
2022-11-05 02:43:57 +0300

    Finish Work was finished.

--- Assets/Code/TaskTwo.cs
+++ Assets/Code/TaskTwo.cs
@@ -19,18 +19,21 @@ namespace SystemProgramming.Lesson2Jobs
 
         [SerializeField] private bool _run;
 
-        [Space][SerializeField] private bool _randomize;
+        [Space]
+        [SerializeField] private bool _randomize;
         [SerializeField] private int _arrayLength;
 
-        [Space][SerializeField] private Vector3[] _positions;
+        [Space]
+        [SerializeField] private Vector3[] _positions;
         [SerializeField] private Vector3[] _velocities;
 
         private NativeArray<Vector3> _positionsNative;
         private NativeArray<Vector3> _velocitiesNative;
         private NativeArray<Vector3> _finalPositions;
 
-        private int _frames;
-        private bool _inProgress;
+        private int _frames;// Счётчик кадров, за которые выполняется задача
+        private bool _inProgress;// Нужен только для подсчёта кадров, за которые выполнится задача
+
         private MyTaskTwo _myTask;
         private JobHandle _jobHandle;
 
@@ -38,15 +41,16 @@ namespace SystemProgramming.Lesson2Jobs
         {
             if (_randomize)
             {
-                _randomize = false;
+                Debug.LogWarning("Clicked Randomize");
                 CreateData();
+                _randomize = false;
             }
 
             if (_run)
             {
-                _run = false;
-                _inProgress = true;
+                Debug.LogWarning("Clicked Run");
                 StartJob();
+                _run = false;
             }
 
             if (_inProgress)
@@ -59,10 +63,20 @@ namespace SystemProgramming.Lesson2Jobs
         {
             if (_jobHandle.IsCompleted)
             {
+                _jobHandle.Complete();
+                // _jobHandle.Complete() совсем не нужен.
+                // Почти совсем.
+                // Если задача выполняется дольше одного кадра, то не нужен.
+                // Если задача выполняется один кадр и в этом же кадре попытаться освободить NativeArray(Dispose),
+                // то без предварительно вызванного jobHandle.Complete() будет ошибка -система будет считать задачу
+                // всё ещё невыполненной, хотя _jobHandle.IsCompleted уже будет в true...
+                // _jobHandle.Complete() перенёс в то место, где освобождаю первый контейнер(сразу же перед ним).
+                Debug.LogWarning("_jobHandle IsCompleted");
                 _inProgress = false;
                 _positionsNative.Dispose();
                 _velocitiesNative.Dispose();
 
+                // Тут основной поток подвисает при больших значениях (100 000)
                 foreach (var item in _finalPositions)
                 {
                     Debug.Log(item);
@@ -79,6 +93,7 @@ namespace SystemProgramming.Lesson2Jobs
 
         private void CreateData()
         {
+            // Тут основной поток подвисает при больших значениях (100 000)
             _positions = new Vector3[_arrayLength];
             _velocities = new Vector3[_arrayLength];
 
@@ -91,10 +106,12 @@ namespace SystemProgramming.Lesson2Jobs
 
         private void StartJob()
         {
+            Debug.LogWarning("Create NativeArray's");
             _positionsNative = new NativeArray<Vector3>(_positions, Allocator.Persistent);
             _velocitiesNative = new NativeArray<Vector3>(_velocities, Allocator.Persistent);
             _finalPositions = new NativeArray<Vector3>(_positions.Length, Allocator.Persistent);
 
+            Debug.LogWarning("Create new MyTaskTwo");
             _myTask = new MyTaskTwo
             {
                 PositionsNative = _positionsNative,
@@ -102,15 +119,20 @@ namespace SystemProgramming.Lesson2Jobs
                 FinalPositions = _finalPositions,
                 DeltaTime = Time.deltaTime,
             };
-
+            
             _frames = Time.frameCount;
+
+            Debug.LogWarning("_myTask Scheduled");
             _jobHandle = _myTask.Schedule(_positions.Length, 32);
-            _jobHandle.Complete();
+            
+            // Раньше тут был _jobHandle.Complete();
+            
+            _inProgress = true;
         }
 
         private void LateUpdate()
         {
-            Debug.LogWarning("Tick");
+            Debug.Log("Tick");// Сигнализатор того, что апдейт не подвис
         }
 
         private struct MyTaskTwo : IJobParallelFor
@@ -123,7 +145,7 @@ namespace SystemProgramming.Lesson2Jobs
 
             public void Execute(int index)
             {
-                //Debug.LogWarning($"Index = {index}");
+                Debug.LogWarning("Execute");// Сигнализатор выполнения задачи. Выводится всегда сразу пачкой
                 FinalPositions[index] = PositionsNative[index] + VelocitiesNative[index] * DeltaTime;
             }
         }
